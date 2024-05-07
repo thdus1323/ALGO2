77be4ca5dbb53a09eee9a71d954020df1c70fa36
SO YEON LEE <thdus1323@gmail.com>
2024-05-08 00:12:23 +0900

    구구단, 별 그리기

--- src/main/java/com/example/gugu/Gugu.java
+++ src/main/java/com/example/gugu/Gugu.java
@@ -4,9 +4,10 @@ public class Gugu {
     public static void main(String[] args) {
         int s = 2;
         int f = 5;
+        //2~5 출력
 
         for (int i = 1; i <= 9; i++){
-            for (int dan = s; dan < f; dan++) {
+            for (int dan = s; dan <= f; dan++) {
                 System.out.printf("%2d * %2d = %3d", dan, i, dan * i);
             }
             System.out.println();
